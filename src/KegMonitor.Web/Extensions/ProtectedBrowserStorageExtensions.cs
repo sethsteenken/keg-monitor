46f7df8ef74d01693f166e3e0eac705198f5c020
Seth Steenken <sethsteenken@gmail.com>
2022-06-23 21:12:07 -0400

    Attempt to persist dark mode selection (WIP)

---
+++ src/KegMonitor.Web/Extensions/ProtectedBrowserStorageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace KegMonitor.Web
+{
+    public static class ProtectedBrowserStorageExtensions
+    {
+        public static async Task<bool> GetOrSetDarkThemeAsync(
+            this ProtectedBrowserStorage storage, bool value)
+        {
+            var result = await storage.GetAsync<bool>("DarkMode");
+            if (result.Success)
+                return result.Value;
+            
+            await storage.SetAsync("DarkMode", value);
+            return value;
+        }
+
+        public static async Task SetDarkThemeAsync(
+            this ProtectedBrowserStorage storage, bool value)
+        {
+            await storage.SetAsync("DarkMode", value);
+        }
+    }
+}
