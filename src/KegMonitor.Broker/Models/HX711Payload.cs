92d47e1410a7d0be207b0cd8c1e95cf5e10c639e
Seth Steenken <sethsteenken@gmail.com>
2023-02-05 18:11:34 -0500

    Removed all obsolete broker application code

--- src/KegMonitor.Broker/Models/HX711Payload.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KegMonitor.Broker
-{
-    internal class HX711Payload
-    {
-        public int Weight { get; set; }
-        public int WeightRaw { get; set; }
-        public int AbsRaw { get; set; }
-    }
-}
