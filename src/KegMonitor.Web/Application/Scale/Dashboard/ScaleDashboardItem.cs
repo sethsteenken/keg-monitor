04724ccf4cbe09993f4941588ce91256f58a827e
Seth Steenken <sethsteenken@gmail.com>
2023-07-14 00:03:21 -0400

    Dashboard - Scale online/offline logic with colored icon

--- src/KegMonitor.Web/Application/Scale/Dashboard/ScaleDashboardItem.cs
+++ src/KegMonitor.Web/Application/Scale/Dashboard/ScaleDashboardItem.cs
@@ -2,6 +2,19 @@
 {
     public class ScaleDashboardItem : ScaleDisplayItem
     {
+        public ScaleSensor Sensor { get; set; } = new ScaleSensor();
         public ScaleWeightMetricsData WeightMetricsData { get; set; }
+
+        public async Task InitializeSensorAsync()
+        {
+            if (WeightMetricsData != null
+                && WeightMetricsData.WeightChangesForDisplay.Any()
+                && WeightMetricsData.WeightChangesForDisplay.Select(w => w.TimeStamp).First() >= DateTime.Now.AddSeconds(-15))
+            {
+                await Sensor.BringOnlineAsync();
+            }
+            else
+                await Sensor.ResetMonitoringTimerAsync();
+        }
     }
 }
---
+++ src/KegMonitor.Web/Application/Scale/ScaleSensor.cs
@@ -0,0 +1,62 @@
+﻿using MudBlazor;
+
+namespace KegMonitor.Web.Application
+{
+    public class ScaleSensor : IAsyncDisposable
+    {
+        public ScaleSensor()
+        {
+            StatusColor = Color.Default;
+            StatusText = "Checking sensor...";
+        }
+
+        public bool Online { get; private set; }
+        public Color StatusColor { get; private set; }
+        public string StatusText { get; private set; }
+        public Timer Timer { get; private set; }
+
+        public Task ResetMonitoringTimerAsync()
+        {
+            const int dueTime = 15000;
+            const int period = 15000;
+
+            if (Timer != null)
+            {
+                Timer.Change(Timeout.Infinite, Timeout.Infinite);
+                Timer.Change(dueTime, period);
+            }
+            else
+            {
+                Timer = new Timer(async (state) =>
+                {
+                    await TakeOfflineAsync();
+                }, null, dueTime, period);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public async Task BringOnlineAsync()
+        {
+            Online = true;
+            StatusColor = Color.Success;
+            StatusText = "Sensor online.";
+            await ResetMonitoringTimerAsync();
+        }
+
+        public Task TakeOfflineAsync()
+        {
+            Online = false;
+            StatusColor = Color.Error;
+            StatusText = "Sensor offline.";
+
+            return Task.CompletedTask;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (Timer != null)
+                await Timer.DisposeAsync();
+        }
+    }
+}
