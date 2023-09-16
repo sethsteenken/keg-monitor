66194ca10c43745920a532533e21dc1a3b96d28c
Seth Steenken <sethsteenken@gmail.com>
2023-09-16 13:58:51 -0400

    Scale - updated Add page to include validation on inputs

--- src/KegMonitor.Web/Application/Scale/Manage/ScaleAddModel.cs
+++ src/KegMonitor.Web/Application/Scale/Manage/ScaleAddModel.cs
@@ -1,8 +1,14 @@
-﻿namespace KegMonitor.Web.Application
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KegMonitor.Web.Application
 {
     public class ScaleAddModel
     {
+        [Required(ErrorMessage = "Id is required.")]
+        [Range(1, 1000, ErrorMessage = "Id must be greater than 0 and less than 1000.")]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Endpoint is required.")]
         public string Endpoint { get; set; }
     }
 }
