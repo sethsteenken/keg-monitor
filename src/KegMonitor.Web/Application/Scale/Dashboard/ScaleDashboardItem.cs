52e18c20898a2139c7cccd479034902b0f9c1fe6
Seth Steenken <sethsteenken@gmail.com>
2023-10-01 11:07:16 -0400

    Dashboard - updated scales to be collapsed by default

--- src/KegMonitor.Web/Application/Scale/Dashboard/ScaleDashboardItem.cs
+++ src/KegMonitor.Web/Application/Scale/Dashboard/ScaleDashboardItem.cs
@@ -1,9 +1,8 @@
-﻿using KegMonitor.Core.Entities;
-
-namespace KegMonitor.Web.Application
+﻿namespace KegMonitor.Web.Application
 {
     public class ScaleDashboardItem : ScaleDisplayItem
     {
         public ScaleWeightMetricsData WeightMetricsData { get; set; }
+        public bool IsExpanded { get; set; } = false;
     }
 }
