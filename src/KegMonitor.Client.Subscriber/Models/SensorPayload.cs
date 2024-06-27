d2d5c23d59d5a387911a90a7c0321245f0abf900
Seth Steenken <sethsteenken@gmail.com>
2024-06-27 13:51:20 -0400

    Removed MQTT test apps

--- src/KegMonitor.Client.Subscriber/Models/SensorPayload.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KegMonitor.Client.Subscriber
-{
-    internal class SensorPayload
-    {
-        public DateTime Time { get; set; }
-        public HX711Payload HX711 { get; set; } = new HX711Payload();
-    }
-}
