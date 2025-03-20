00832647a1f51df245e70dd104cbdf3adc5cadd1
Seth Steenken <sethsteenken@gmail.com>
2025-03-19 22:11:07 -0400

    Config - cleanup of mudblazor config call

---
+++ src/KegMonitor.Web/Extensions/MudBlazorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using MudBlazor;
+
+namespace KegMonitor.Web
+{
+    public static class MudBlazorExtensions
+    {
+        public static SnackbarConfiguration SetConfigValues(this SnackbarConfiguration config)
+        {
+            config.PositionClass = Defaults.Classes.Position.BottomCenter;
+            config.PreventDuplicates = false;
+            config.NewestOnTop = false;
+            config.ShowCloseIcon = true;
+            config.VisibleStateDuration = 10000;
+            config.HideTransitionDuration = 500;
+            config.ShowTransitionDuration = 500;
+            config.SnackbarVariant = Variant.Filled;
+
+            return config;
+        }
+    }
+}
--- src/KegMonitor.Web/Program.cs
+++ src/KegMonitor.Web/Program.cs
@@ -17,15 +17,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices(config =>
 {
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
-
-    config.SnackbarConfiguration.PreventDuplicates = false;
-    config.SnackbarConfiguration.NewestOnTop = false;
-    config.SnackbarConfiguration.ShowCloseIcon = true;
-    config.SnackbarConfiguration.VisibleStateDuration = 10000;
-    config.SnackbarConfiguration.HideTransitionDuration = 500;
-    config.SnackbarConfiguration.ShowTransitionDuration = 500;
-    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+    config.SnackbarConfiguration.SetConfigValues();
 });
 
 // added to support signalr client
